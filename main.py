faed017f64e43b5d78fc75126c9f2e568fecc913
nanoshimarobot <shimada.toyozo.lf@tut.jp>
2025-07-31 17:48:45 +0900

    a

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
 from transformers import AutoImageProcessor, AutoModel
 from PIL import Image
+from ultralytics import YOLO
+from ultralytics.engine.results import Results
 
 import torch
+import cv2
 import numpy as np
 
 if __name__ == "__main__":
+    img_name = "hallway"
+
+    detector = YOLO("yolo11x-seg.pt")
+    detector.fuse()
+    detect_res_list: list[Results] = detector.predict(
+        source=f"data/{img_name}.png",
+        save=False,
+        verbose=False,
+        stream=False,
+        device="cuda",
+        conf=0.5,
+        half=True,
+    )
+    detect_res = detect_res_list[0]
+    
+    # save annotated image
+    annotated_img = detect_res.plot()
+    cv2.imwrite(f"data/{img_name}_annotated.png", annotated_img)
+
+    # dinov2 feature extraction
     proc = AutoImageProcessor.from_pretrained("facebook/dinov2-base")
     vit = AutoModel.from_pretrained("facebook/dinov2-base").eval().to("cuda")
 
-    hallway_img = Image.open("/home/toyozoshimada/dinov2_test/data/hallway.png").convert("RGB")
-    lunchroom_img = Image.open("/home/toyozoshimada/dinov2_test/data/lunchroom.png").convert("RGB")
-
+    hallway_img = Image.open(f"data/{img_name}.png").convert("RGB")
+    print(hallway_img.size)  # (Width, Height)
     input = proc(hallway_img, return_tensors="pt").to("cuda")
-    # print(input)
+    print(input["pixel_values"].shape)  # (Batch, Channel, Height, Width)
+    # print(outputs.last_hidden_state.shape)  # (Batch, TokenSize(CLS + Patch), EmbeddingSize)
     with torch.no_grad():
         outputs = vit(**input)
 
-    print(outputs.last_hidden_state.shape)  # (1, 197, 768)
+    patch_tokens = outputs.last_hidden_state[:, 1:]
+    pa
